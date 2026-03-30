75e9dfc2f83aeb6211ca14d1f839f7eafe311fdc
Wojtek Bażant <wojciech.bazant+ebi@gmail.com>
2026-03-30 21:21:08 +0100

    Aider prompt: Make a simple UI line for each location. I want a line with first types (like in DiaryEntry), then address. You have type_ids and address field on the JSON. Remove the stringified JSON. Types should link to location page.

--- src/components/saved/SavedLocationsPage.js
+++ src/components/saved/SavedLocationsPage.js
@@ -1,10 +1,68 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { Link,useParams } from 'react-router-dom'
+import styled from 'styled-components'
 
 import { fetchLists, fetchLocationsForList } from '../../redux/saveSlice'
 import { BackButton } from '../ui/ActionButtons'
 import { Page } from '../ui/PageTemplate'
+
+const LocationList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+`
+
+const LocationItem = styled.li`
+  padding: 0.5rem 0;
+  border-bottom: 1px solid ${({ theme }) => theme.secondaryBackground};
+`
+
+const LocationLink = styled(Link)`
+  color: ${({ theme }) => theme.blue} !important;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+const ScientificName = styled.span`
+  font-style: italic;
+`
+
+const CommonName = styled.span`
+  font-weight: bold;
+`
+
+const Address = styled.span`
+  color: ${({ theme }) => theme.secondaryText};
+  margin-left: 0.5rem;
+`
+
+const LocationTypesList = ({ location }) => {
+  const typeElements = (location.type_ids || []).map((typeId, idx) => {
+    if (typeId.commonName) {
+      return <CommonName key={idx}>{typeId.commonName}</CommonName>
+    }
+    if (typeId.scientificName) {
+      return <ScientificName key={idx}>{typeId.scientificName}</ScientificName>
+    }
+    return <span key={idx}>{typeId}</span>
+  })
+
+  const typesWithSeparators = typeElements.reduce((prev, curr, idx) => {
+    if (prev.length) {
+      return [...prev, <span key={`sep-${idx}`}>, </span>, curr]
+    }
+    return [curr]
+  }, [])
+
+  return (
+    <LocationLink to={`/locations/${location.id}`}>
+      {typesWithSeparators.length > 0 ? typesWithSeparators : location.id}
+    </LocationLink>
+  )
+}
 
 const SavedLocationsPage = () => {
   const dispatch = useDispatch()
@@ -53,7 +111,14 @@ const SavedLocationsPage = () => {
       ) : currentList.locationIds.length === 0 ? (
         <p>No locations saved in this list.</p>
       ) : (
-        <pre>{JSON.stringify(currentListLocations, null, 2)}</pre>
+        <LocationList>
+          {(currentListLocations || []).map((location) => (
+            <LocationItem key={location.id}>
+              <LocationTypesList location={location} />
+              {location.address && <Address>{location.address}</Address>}
+            </LocationItem>
+          ))}
+        </LocationList>
       )}
     </Page>
   )
