23452ea9fcbf0ce3808bdf87f8e92f8af0658be1
Wojtek Bażant <wojciech.bazant+ebi@gmail.com>
2026-03-31 11:37:07 +0100

    Aider prompt: The icon doesn't show up. Also make the button narrower

--- src/components/saved/SavedLocationsPage.js
+++ src/components/saved/SavedLocationsPage.js
@@ -155,12 +155,15 @@ const SavedLocationsPage = () => {
               </LocationInfo>
               <Button
                 secondary
-                leftIcon={<i className="bx bx-minus" />}
                 onClick={() => handleRemove(location.id)}
                 disabled={isListBusy}
                 aria-label={`Remove location ${location.id} from list`}
+                style={{ padding: '0 6px', minWidth: 0 }}
               >
-                Remove
+                <i
+                  className="bx bx-x"
+                  style={{ fontSize: '1.2em', lineHeight: 1 }}
+                />
               </Button>
             </LocationItem>
           ))}
